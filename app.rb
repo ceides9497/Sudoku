96eea16ad8e8d0ba09c6db2b23d35a217c1df40e
César Ivan Delgado Silva <ceides9497@gmail.com>
2016-11-20 23:47:04 -0400

    Probando en vistas - FAIL
    No funciona

--- app.rb
+++ app.rb
@@ -5,15 +5,15 @@ get '/' do
   erb :index
 end
 
-post '/generar' do
-  valor = params[:num].to_i
-  sudo = Sudoku.new
-  resp= sudo.verify(valor)
-  "<center><strong>respuesta: </strong>"+resp+"</center>"
-end
-
 post '/gettable' do
 	sudoku = Sudoku.new
 	@sudokutable = sudoku.initTable()
 	erb :game
+end
+
+post '/generar' do
+  valor = params[:cell].to_i
+  sudo = Sudoku.new
+  resp= sudo.is_number_between_1_to_9?(valor)
+  "<center><strong>respuesta: </strong>"+resp+"</center>"
 end
