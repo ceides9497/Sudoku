2853b0e49e42d08b063915c09db469ab2f3f07e8
César Ivan Delgado Silva <ceides9497@gmail.com>
2016-11-26 02:28:41 -0400

    Arreglos

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
 require_relative 'lib/Sudoku.rb'
 
+def for_message(table1)
+  sudoku = Sudoku.new
+  for numfil in 0..8
+    for numcol in 0..8
+      if table1[numfil][numcol] != 0
+        if not sudoku.is_number_factible?(table1[numfil][numcol],numfil,numcol,table1)
+          return false
+        end
+      end
+    end
+  end
+  return true
+end
+
 get '/' do
   erb :index
 end
@@ -19,5 +33,8 @@ post '/generar' do
   @sudokutable = sudo.initTable()
   @currenttable = sudo.array_from_1d_to_2d(valores)
   @booleantable = sudo.check_table(valores)
+  @solutionTable=sudo.getSolutionTable()
+#  @solved = sudo.is_table_correct_completed?(@currenttable,@solutionTable)
+  @solved = for_message(@currenttable)
   erb :game
 end
